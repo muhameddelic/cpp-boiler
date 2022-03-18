b56d3ea93309436a2b876490a6e76adae749d088
Muhamed Delic <muhamed.delicc@live.com>
2022-03-19 10:43:37 +1100

    Fix linux & macos builds

--- cmake/SetupGoogleTest.cmake
+++ cmake/SetupGoogleTest.cmake
@@ -7,5 +7,6 @@ FetchContent_Declare(
 if (${CMAKE_SYSTEM_NAME} MATCHES Windows)
   # For Windows: Prevent overriding the parent project's compiler/linker settings
   set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-  FetchContent_MakeAvailable(googletest)
 endif()
+
+FetchContent_MakeAvailable(googletest)
